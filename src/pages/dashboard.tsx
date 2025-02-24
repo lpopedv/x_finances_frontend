845b59ebe02aa5978331762a4a7505ec4bf286bd
Lucas Pope <lpopedv@proton.me>
2025-02-24 16:03:14 -0300

    feat: implement 'Dívidas recorrentes' in dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,32 @@
+import { useQuery } from "@tanstack/react-query"
+import { formatPriceInReais } from "../utils"
+
 export const Dashboard = () => {
+
+  const { data: dashboardData } = useQuery({
+    queryKey: ['getDashboardData'],
+    queryFn: getDashboardData
+  })
+
   return (
     <div>
       <h1>Dashboard</h1>
+
+      <div>
+        <p>Dívidas recorrentes</p>
+        <p>{formatPriceInReais(dashboardData?.totalFixedExpenses)}</p>
+      </div>
     </div>
   )
+}
+
+const getDashboardData = async () => {
+  const response = await fetch(`http://localhost:3333/dashboard-data`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return await response.json()
 }
