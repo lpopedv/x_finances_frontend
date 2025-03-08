62c8db1281d2cd33801aaf5ea8e56888eee4570b
Lucas Pope <lpopedv@proton.me>
2025-03-07 22:48:10 -0300

    feat: add tsconfigPaths vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -6,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tsconfigPaths(),
+    tailwindcss(),
   ],
 })
